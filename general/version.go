f33408795b2126d8b1b6749a568b87a37a253097
YJ-ArchLinux-Laptop <yj1516268@outlook.com>
2026-03-12 17:42:38 +0800

    v0.0.2

--- general/version.go
+++ general/version.go
@@ -16,7 +16,7 @@ import (
 
 const (
 	Name    string = "Mqttc"                 // 程序名
-	Version string = "v0.0.1"                // 程序版本
+	Version string = "v0.0.2"                // 程序版本
 	Project string = "github.com/yhyj/mqttc" // 项目地址
 )
 
